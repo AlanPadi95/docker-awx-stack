aa1ca22c31cee8686d962f38aa83dd44356ef687
Alan Padierna <alanpadierna9595@gmail.com>
2020-04-09 17:13:32 +0200

    new changes from ansible templates

--- config/credentials.py
+++ config/credentials.py
@@ -10,25 +10,4 @@ DATABASES = {
     }
 }
 
-BROKER_URL = 'amqp://{}:{}@{}:{}/{}'.format(
-    "guest",
-    "changeme",
-    "rabbitmq",
-    "5672",
-    "awx")
-
-CHANNEL_LAYERS = {
-    'default': {'BACKEND': 'asgi_amqp.AMQPChannelLayer',
-                'ROUTING': 'awx.main.routing.channel_routing',
-                'CONFIG': {'url': BROKER_URL}}
-}
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '{}:{}'.format("memcached", "11211")
-    },
-    'ephemeral': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-    },
-}
+BROADCAST_WEBSOCKET_SECRET = "awx"
